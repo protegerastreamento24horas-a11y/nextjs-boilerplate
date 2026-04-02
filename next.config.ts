d2b503e3aceeec7ba43eec2e69c27909248ee71d
iago <protegerastreamento24horas@gmail.com>
2026-04-02 20:30:57 -0300

    fix: mover config body parser para API route

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
-    },
-  },
 };
 
 export default nextConfig;
